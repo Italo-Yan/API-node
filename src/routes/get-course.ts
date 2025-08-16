351b598123c9f40326ee1933dcd2a3a61a810340
Italo-Yan <italoyandev@gmail.com>
2025-08-16 16:50:19 -0300

    feat: adding pagination method

--- src/routes/get-course.ts
+++ src/routes/get-course.ts
@@ -1,6 +1,7 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { db } from '../database/client.ts'
 import { courses } from '../database/schema.ts'
+import { ilike, asc, and, SQL } from 'drizzle-orm'
 import z from 'zod'
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
@@ -8,6 +9,11 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
     schema: {
       tags: ['Courses'],
       summary: 'Get all courses',
+      querystring: z.object({
+        search: z.string().optional(),
+        orderBy: z.enum(['id', 'title']).optional().default('id'),
+        page: z.coerce.number().optional().default(1),
+      }),
       description: 'This route returns all courses',
       response: {
           200: z.object({
@@ -16,16 +22,33 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
               id: z.string(),
               title: z.string()
             })
-          )
+          ),
+          total: z.number()
         }).describe('Courses returned successfully')
       }
     }
   }, async (request, reply) => {
-    const result = await db.select({
-      id: courses.id,
-      title: courses.title,
-  }).from(courses)
-  
-    return reply.send({ courses: result })
+    const { search, orderBy, page } = request.query
+
+    const conditions: SQL[] = []
+
+    if (search) {
+      conditions.push(ilike(courses.title, `%${search}%`))
+    }
+
+    const [result, total] = await Promise.all([
+      db
+        .select({
+          id: courses.id,
+          title: courses.title,
+        })
+        .from(courses)
+        .orderBy(asc(courses[orderBy]))
+        .offset((page - 1) * 2)
+        .limit(2)
+        .where(and(...conditions)),
+      db.$count(courses, and(...conditions))
+    ])
+    return reply.send({ courses: result, total })
   })
 }
